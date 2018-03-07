ea76d211748e8f34c07ce08ac2c6dc77a3afd251
Administrator <Administrator@MS-20120726SXME>
2018-03-07 20:52:43 +0800

    测试一下服务器的git pull功能；

---
+++ control/model/gittest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace model;
+
+/**
+ * Description of gittest
+ *
+ * @author Kevin
+ */
+class gittest {
+    
+    //put your code here
+}
