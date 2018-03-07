8b01f8e4ebc8fb837adb1a0b8877f56b47974618
Administrator <Administrator@MS-20120726SXME>
2018-03-07 23:01:56 +0800

    测试一下服务器的git pull功能3333

---
+++ control/model/testgit2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace model;
+
+/**
+ * Description of testgit2
+ *
+ * @author Kevin
+ */
+class testgit2 {
+    //put your code here
+}
