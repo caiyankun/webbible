bee9e2910b728afc51cb0f0ccf4c151325886803
colin <coliner_sun@163.com>
2018-03-13 22:28:41 +0800

    添加用户行为

--- more.sanmantech.com/model/useraction.php
+++ more.sanmantech.com/model/useraction.php
@@ -16,23 +16,35 @@ class useraction
      * @param $action
      * @param $target
      */
-    public function add(){
+    public function add($action,$target){
         \User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！");
-        $content="";
-        foreach ($_GET as $key => $value){
-            if($key=='uid'){
-            } else {
-                if(empty($content)){
-                    $content=$content.$key."='".$value."'";
-                } else {
-                    $content=$content.",".$key."='".$value."'";
-                }
-            }
-        }
-        if(!\Db::simplecall("more.useractionadd", array(\User::uid(),$content))){
+        if(!\Db::simplecall("more.useractionadd", array(\User::uid(),$action,$target))){
             \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
         } else {
+            \Response::returntaskok(\Db::tabledata());
+        }
+    }
+
+    /**
+     * 查询行为
+     * @param $action
+     */
+    public function query($action){
+
+        \User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！");
+        if(!\Db::simplecall("more.useractionquery", array(\User::uid(),$action))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        }else{
             \Response::returntaskok(\Db::arraydata());
+        }
+    }
+
+    public function del($action,$target){
+        \User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！");
+        if(!\Db::simplecall("more.useractiondel", array(\User::uid(),$action,$target))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        }else{
+            \Response::returntaskok("取消成功！");
         }
     }
 }
