61aeca47f39ed4d470ee674bfad73c054dcd2f5d
colin <coliner_sun@163.com>
2018-03-14 22:46:12 +0800

    添加设计师php

--- more.sanmantech.com/model/member.php
+++ more.sanmantech.com/model/member.php
@@ -23,4 +23,41 @@ class member
         }
     }
 
+    /**
+     * 充值
+     * @param $value
+     */
+    public function reload($value){
+        \User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！");
+        if(!\Db::simplecall("more.memberdereload", array(\User::uid(),$value))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        }else{
+            \Response::returntaskok(\Db::arraydata());
+        }
+    }
+
+    public function deposit($value){
+        if(\User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！")){
+            if(!\Db::simplecall("more.memberdeposit", array(\User::uid(),$value))){
+                \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+            }else{
+                \Response::returntaskok(\Db::arraydata());
+            }
+        }
+        if(\User::checkright(800)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！"))
+            if(!\Db::simplecall("more.memberdeposit", array(\User::uid(),$value))){
+                \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+            }else{
+                \Response::returntaskok('退押金成功！');
+            }
+    }
+
+    public function update($membertype){
+        \User::checkright(100)||\Response::returntaskfail("您还未登录，请先登录！！",2,"您还未登录，请先登录！");
+        if(!\Db::simplecall("more.membercreate", array(\User::uid(),$membertype))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        }else{
+            \Response::returntaskok(\Db::arraydata());
+        }
+    }
 }
