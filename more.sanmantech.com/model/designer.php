cfa3a5db9a6ce4f43c47f1e13415e6e1c547bb37
colin <coliner_sun@163.com>
2018-03-14 22:49:27 +0800

    添加设计师

---
+++ more.sanmantech.com/model/designer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/14
+ * Time: 22:38
+ */
+
+namespace model;
+
+
+class designer {
+    public function creatework($productinfo){
+
+    }
+
+}
