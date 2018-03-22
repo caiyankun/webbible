5685275b5fd55a677b86a96a954bde63749e2a5e
Kevin Cai <kevin.cai@sanmantech.com>
2018-03-22 21:16:27 +0800

    修正产品状态代码

--- more.sanmantech.com/model/product.php
+++ more.sanmantech.com/model/product.php
@@ -10,8 +10,44 @@ namespace model;
 
 
 class product {
+    public function staticupd($pid,$oldstat,$newstat,$qty) {
+        if(!\Db::simplecall("more.productstatupd", array($pid,$oldstat,$newstat,$qty, \User::uid()))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        } else {
+            \Response::returntaskok(\Db::tabledata());
+        }   
+    }
     public function stat($filterinfo=""){
-        
+        if(is_string($filterinfo)){
+            try {
+                $filterinfo= json_decode($filterinfo);
+            } catch (Exception $exc) {
+            }
+        }
+       !is_object($filterinfo)&&!is_array($filterinfo)&&($filterinfo=[]);
+        $filterstr="1=1";
+        foreach ($filterinfo as $key => $value){
+            if($filterstr=="1=1"){
+                //$filterstr=$key;
+                if(!empty($value)&&is_string($value)){
+                        $filterstr=$key."=\'".$value."\'";
+                } elseif (!empty ($value)&&is_array($value)){
+                    $filterstr=$key." in (\'".implode("\',\'",$value)."\') ";
+                }
+            }else {
+                if(!empty($value)&&is_string($value)){
+                        $filterstr=$filterstr." AND ".$key."=\'".$value."\'";
+                } elseif (!empty ($value)&&is_array($value)){
+                    $filterstr=$filterstr." AND ".$key." in (\'".implode("\',\'",$value)."\') ";
+                }                
+            }
+        }
+        //filter 功能暂未在mysql中实现
+        if(!\Db::simplecall("more.productstat", array($filterstr))){
+            \Response::returntaskfail("存储过程调用失败！",\Db::$error,\Db::$info);
+        } else {
+            \Response::returntaskok(\Db::cubedata());
+        }    
     }
     public function query($filterinfo,$orderinfo,$page=1,$length=2) {
         if(is_string($filterinfo)){
